f1cb98ca84e642680ed253a4b48ca6314e7907f9
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2018-08-14 20:36:51 -0300

    DateHelper, blindando o array, ListModel

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,33 +1,39 @@
-class NegociacaoController{
+class NegociacaoController {
 
-    constructor(){
+    constructor() {
         let $ = document.querySelector.bind(document);
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
-    adiciona(event){
-       event.preventDefault();
+    adiciona(event) {
+        event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegocicao());
+        this._limpaFormulario();
 
-        let data = new Date(
-            ...this._inputData.value.split('-')
-            .map((item,indice) => item - item %2 ));
-        
-        let negociacao = new Negociacao(
-            data,
+        console.log(this._listaNegociacoes.negociacoes);
+        //Expressão Regular
+        //let data = new Date(this._inputData.value.replace(/-/,','));
+
+        //SPLIT
+        //let data = new Date(this._inputData.value.split('-'));
+
+
+    }
+    _criaNegocicao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
 
-        console.log(negociacao);
-        //Expressão Regular
-        //let data = new Date(this._inputData.value.replace(/-/,','));
-        
-        //SPLIT
-        //let data = new Date(this._inputData.value.split('-'));
-       
-       console.log(data);
-        
+        this._inputData.focus();
     }
 }
