847f46262ea49c3608f17632ff30ced60f96ff3d
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2018-08-14 22:27:07 -0300

    Template String

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,11 +6,15 @@ class NegociacaoController {
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacaoView($("#negociacoesView"));
+
+        this._negociacoesView.update(this._listaNegociacoes);
 
     }
     adiciona(event) {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegocicao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
 
         console.log(this._listaNegociacoes.negociacoes);
