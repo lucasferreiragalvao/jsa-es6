99f596f7da4a5c8226f44369a49c9d104b152930
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2018-08-16 20:11:34 -0300

    Mensagem,Herança

--- aluraframe/client/js/app/views/NegociacaoView.js
+++ aluraframe/client/js/app/views/NegociacaoView.js
@@ -1,10 +1,10 @@
-class NegociacaoView {
+class NegociacaoView extends View{
 
     constructor(elemento) {
-        this._elemento = elemento;
+        super(elemento);
     }
 
-    _template(model) {
+    template(model) {
         return `
             <table class="table table-hover table-bordered">
             <thead>
@@ -35,9 +35,5 @@ class NegociacaoView {
             </tfoot>
             </table>
         `;
-    }
-
-    update(model) {
-        this._elemento.innerHTML = this._template(model);
     }
 }
